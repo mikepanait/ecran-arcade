902ff40f5bcfe3591fe6a6bfb78821aee35890ab
mikepanait <mike.panait@icloud.com>
2025-06-17 10:52:01 +0300

    Add files via upload

--- ecranalbastrucaremerge.ino
+++ ecranalbastrucaremerge.ino
@@ -1,22 +1,19 @@
-#include <TFT_eSPI.h>  // use this lib, not Adafruit
+#include <TFT_eSPI.h>
 #include <SPI.h>
+#include "Pixelmare_little_endian.h"  // header file with #define and PixelmareImage[]
 
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
   Serial.begin(115200);
   tft.init();
-  tft.setRotation(0);
+  tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setTextSize(2);
-  tft.setCursor(10, 10);
-  tft.println("JMD-PS130 V2.0");
-  tft.fillRect(20, 50, 50, 50, TFT_RED);
-  tft.fillRect(80, 50, 50, 50, TFT_GREEN);
-  tft.fillRect(140, 50, 50, 50, TFT_BLUE);
+
+  tft.setSwapBytes(true);  // For little-endian RGB565 data
+
+  // Cast image data to uint16_t* since it's declared as uint8_t[]
+  tft.pushImage(0, 0, PixelmareImage_WIDTH, PixelmareImage_HEIGHT, (uint16_t*)PixelmareImage);
 }
 
-void loop() {
-  // Nothing here for now
-}
+void loop() {}
